a91377982d4a2a264b9bf76de936bb95c9611ced
illies douhab <illies.douhab.etu@univ-lille.fr>
2024-12-20 07:49:48 +0100

    script amelioration

--- osint_finder.sh
+++ osint_finder.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
 
 
 
+if [[ "$1" -eq "" ]];then
+	echo "./osint_finder.sh [username]"
+fi
 
-curl -s -o /dev/null -w  "%{http_code}\n" https://github.com/Douhab-illies
+
+USERNAME="$1"
+
+
+for i in $(cat ./list);do
+	URL=$(echo $i | sed -e "s/REPLACE/$USERNAME/g")
+	CODE_REQ=$(curl -s -o /dev/null -w  "%{http_code}\n" $URL);
+	if [[ "$CODE_REQ" == "200" ]];then
+		echo "[+] $URL"
+	fi
+
+done
