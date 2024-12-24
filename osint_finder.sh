5e589a66c3d82fa9365b0d53e04ff95e1742b57d
illies douhab <illies.douhab.etu@univ-lille.fr>
2024-12-24 21:40:31 +0100

    warning message

--- osint_finder.sh
+++ osint_finder.sh
@@ -10,7 +10,14 @@ fi
 
 USERNAME="$1"
 
+clear
 
+RED='\e[1;31m'
+echo -e "${RED} SOME RESULTS MAY BE FALSE POSITIVES"
+
+
+NOCOLOR='\033[0m'
+echo -e "${NOCOLOR}"
 for i in $(cat ./list);do
 	URL=$(echo $i | sed -e "s/REPLACE/$USERNAME/g")
 	CODE_REQ=$(curl -s -o /dev/null -w  "%{http_code}\n" $URL);
